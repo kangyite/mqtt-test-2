f6ee8091e01fef9e32505b354a330be1b6441386
kangyite <kangyte@gmail.com>
2023-08-08 16:37:15 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <MQTT.h>
+
+const char *ssid = "IP9tcN32kd"; // Enter your Wi-Fi name
+const char *pass = "H$u3e*X3-uByaL=BTXw_r45t&";  // Enter Wi-Fi password
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "public", "public")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
+  // by Arduino. You need to set the IP address directly.
+  client.begin("broker.emqx.io",1883, net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    unsigned long a = millis();
+    client.publish("/hello", "world");
+    unsigned long b = millis();
+    Serial.println(b-a);
+  }
+}
